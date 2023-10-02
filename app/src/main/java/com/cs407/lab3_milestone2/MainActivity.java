bb8ecddb3bbc967472e696fd596eecc23bd5888f
inglettronald <inglettronald@gmail.com>
2023-10-02 05:29:33 -0500

    implement milestone 2

--- app/src/main/java/com/cs407/lab3_milestone2/MainActivity.java
+++ app/src/main/java/com/cs407/lab3_milestone2/MainActivity.java
@@ -1,8 +1,10 @@
 package com.cs407.lab3_milestone2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        Button firstFragmentButton = findViewById(R.id.firstFragmentButton);
+        Button secondFragmentButton = findViewById(R.id.secondFragmentButton);
+
+        firstFragmentButton.setOnClickListener( view ->
+            fragmentManager.beginTransaction()
+                    .replace(R.id.fragmentContainerView, FirstFragment.class, null)
+                    .setReorderingAllowed(true)
+                    .addToBackStack("showing First")
+                    .commit()
+        );
+
+        secondFragmentButton.setOnClickListener( view ->
+            fragmentManager.beginTransaction()
+                    .replace(R.id.fragmentContainerView, SecondFragment.class, null)
+                    .setReorderingAllowed(true)
+                    .addToBackStack("showing First")
+                    .commit()
+        );
     }
 }
